8bfcd9df52bd1468933f6565b13c23d499c9c39c
Astrid <github@astrid.exposed>
2023-12-26 19:18:46 +0100

    Undo 'better searching', easier like this

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,180 +12,124 @@ import JSONPretty from "react-json-pretty"; // https://github.com/chenckang/reac
 import jsonTheme from "react-json-pretty/themes/acai.css";
 
 function starmap(location, system) {
-	const params = {};
-	if (location) params.location = location;
-	if (system) params.system = system;
-	return "https://robertsspaceindustries.com/starmap?" + new URLSearchParams(params);
+  const params = {};
+  if (location) params.location = location;
+  if (system) params.system = system;
+  return (
+    "https://robertsspaceindustries.com/starmap?" + new URLSearchParams(params)
+  );
 }
 
 export default function Index() {
-	const [jsonFetched, setJsonFetched] = useState(false);
-	const [rawJson, setRawJson] = useState(null);
-	const [json, setJson] = useState({ message: "Hold on! Fetching starmap data." });
-	const [inputDelay, setInputDelay] = useState(false);
-	const [previousInput, setPreviousInput] = useState(null);
-	const [resultText, setResultText] = useState("");
+  const [jsonFetched, setJsonFetched] = useState(false);
+  const [rawJson, setRawJson] = useState(null);
+  const [json, setJson] = useState({
+    message: "Hold on! Fetching starmap data.",
+  });
+  const [inputDelay, setInputDelay] = useState(false);
+  const [previousInput, setPreviousInput] = useState(null);
+  const [resultText, setResultText] = useState("");
 
-	useEffect(() => {
-		fetch("https://raw.githubusercontent.com/robertsspaceindustries/sc-starmap/main/out/starmap.json")
-			.then(async (res) => {
-				if (res.ok) {
-					const starmapJson = await res.json();
+  useEffect(() => {
+    if (!jsonFetched)
+      fetch(
+        "https://raw.githubusercontent.com/robertsspaceindustries/sc-starmap/main/out/starmap.json"
+      )
+        .then(async (res) => {
+          if (res.ok) {
+            const starmapJson = await res.json();
 
-					const systems = starmapJson.systems.map((system) => ({
-						...system,
-						__starmap: starmap(system.code),
-					}));
+            const systems = starmapJson.systems.map((system) => ({
+              ...system,
+              __starmap: starmap(system.code),
+            }));
 
-					const objects = starmapJson.objects.map((object) => ({
-						...object,
-						__starmap: starmap(object.code, object.star_system?.code),
-					}));
+            const objects = starmapJson.objects.map((object) => ({
+              ...object,
+              __starmap: starmap(object.code, object.star_system?.code),
+            }));
 
-					setRawJson({ systems, objects });
-					setJson([]);
-					setJsonFetched(true);
-				} else
-					setJson({
-						message: "Failed to fetch starmap data.",
-						code: res.status,
-					});
-			})
-			.catch((err) =>
-				setJson({
-					message: "There was an error fetching starmap data.",
-					error: err,
-				}),
-			);
-	}, [setJsonFetched, setRawJson, setJson]);
+            setRawJson({ systems, objects });
+            setJson([]);
+            setJsonFetched(true);
+          } else
+            setJson({
+              message: "Failed to fetch starmap data.",
+              code: res.status,
+            });
+        })
+        .catch((err) =>
+          setJson({
+            message: "There was an error fetching starmap data.",
+            error: err,
+          })
+        );
+  }, [jsonFetched, setJsonFetched, setRawJson, setJson]);
 
-	async function search(params) {
-		function e(object, args) {
-			let value = object;
-			if (!value) return null;
-			for (const key of args) {
-				value = value[key];
-				if (!value) return null;
-			}
-			return value;
-		}
+  function onSearch(event) {
+    let timeout;
 
-		const newJson = [
-			...rawJson.systems.filter((system) => {
-				for (const paramKey of Object.keys(params)) {
-					const { options: paramOptions, value: paramValue } = params[paramKey];
+    if (!inputDelay) {
+      setInputDelay(true);
 
-					if (paramOptions.includes("includes")) {
-						const realValue = e(system, paramKey.split("."))?.toLowerCase();
-						const includes = realValue?.includes(paramValue.toLowerCase());
-						if (!includes) return false;
-					} else {
-						const realValue = e(system, paramKey.split("."))?.toLowerCase();
-						if (realValue !== paramValue.toLowerCase()) return false;
-					}
-				}
+      // Wait a bit before filtering
+      timeout = setTimeout(async () => {
+        const input = event.target.value;
 
-				return true;
-			}),
-			...rawJson.objects.filter((object) => {
-				for (const paramKey of Object.keys(params)) {
-					const { options: paramOptions, value: paramValue } = params[paramKey];
+        if (input !== previousInput) setPreviousInput(input);
 
-					if (paramOptions.includes("includes")) {
-						const realValue = e(object, paramKey.split("."))?.toLowerCase();
-						const includes = realValue?.includes(paramValue.toLowerCase());
-						if (!includes) return false;
-					} else {
-						const realValue = e(object, paramKey.split("."))?.toLowerCase();
-						if (realValue !== paramValue.toLowerCase()) return false;
-					}
-				}
+        setJson(
+          [
+            ...rawJson.systems.filter(
+              (system) =>
+                system.code?.includes(input.toUpperCase()) ||
+                system.name?.toLowerCase().includes(input.toLowerCase())
+            ),
+            ...rawJson.objects.filter(
+              (object) =>
+                object.name?.toLowerCase().includes(input.toLowerCase()) ||
+                object.designation
+                  ?.toLowerCase()
+                  .includes(input.toLowerCase()) ||
+                object.code?.includes(input.toUpperCase())
+            ),
+          ].filter((v) => typeof v === "object")
+        );
 
-				return true;
-			}),
-		].filter((v) => typeof v === "object");
+        if (typeof json?.length === "number")
+          setResultText(`${json.length} result${json.length === 1 ? "" : "s"}`);
 
-		setJson(newJson);
+        setInputDelay(false);
+      }, 100);
+    }
 
-		if (typeof newJson?.length === "number")
-			setResultText(`${newJson.length} result${newJson.length === 1 ? "" : "s"}`);
-	}
+    return () => clearTimeout(timeout);
+  }
 
-	function onSearch(event) {
-		let timeout;
-
-		if (!inputDelay) {
-			setInputDelay(true);
-
-			// Wait a bit before filtering
-			timeout = setTimeout(async () => {
-				const input = event.target.value;
-
-				if (input !== previousInput) {
-					setPreviousInput(input);
-				}
-
-				const params = {};
-
-				for (const match of [
-					...input.matchAll(
-						/(?<key>[\w_.]+)(:|=)(?<options>[~]?)("(?<valueSurrounded>.*?)"|(?<valuePlain>\w+))/g,
-					),
-				]) {
-					const key = match.groups.key;
-					const options = match.groups.options;
-					const value = (match.groups.valueSurrounded ?? match.groups.valuePlain).replaceAll("\\\\", "");
-
-					params[key] = {
-						options: options
-							? options
-									.trim()
-									.split("")
-									.map((option) => ({ "~": "includes" }[option.trim()]))
-									.filter((v) => v) // Is not falsy
-							: [],
-						value,
-					};
-				}
-
-				if (Object.keys(params).length === 0) return;
-
-				await search(params);
-
-				setInputDelay(false);
-			}, 100);
-		}
-
-		return () => clearTimeout(timeout);
-	}
-
-	return (
-		<>
-			<Head>
-				<title>Starmap Data</title>
-			</Head>
-			<main className={styles.main}>
-				<div className={styles.content}>
-					<a
-						className={styles.searchQueryHelp}
-						target="_blank"
-						href="https://github.com/robertsspaceindustries/sc-starmap-search/blob/main/docs/Search_queries.md"
-					>
-						Search query help
-					</a>
-					<div className={styles.inputContainer}>
-						<input
-							onChange={onSearch}
-							className={styles.input}
-							placeholder="Search query"
-							type="text"
-							disabled={!jsonFetched}
-						/>
-						<span className={styles.inputResults}>{resultText}</span>
-					</div>
-					<JSONPretty data={json} className={styles.result} theme={jsonTheme}></JSONPretty>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Starmap Data</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.inputContainer}>
+            <input
+              onChange={onSearch}
+              className={styles.input}
+              placeholder="Search"
+              type="text"
+              disabled={!jsonFetched}
+            />
+            <span className={styles.inputResults}>{resultText}</span>
+          </div>
+          <JSONPretty
+            data={json}
+            className={styles.result}
+            theme={jsonTheme}
+          ></JSONPretty>
+        </div>
+      </main>
+    </>
+  );
 }
